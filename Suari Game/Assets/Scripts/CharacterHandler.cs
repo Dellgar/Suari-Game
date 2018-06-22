1910423ef03b5d41b9af1ed9350eae46776d4ab4
Dellgar <kotanico@gmail.com>
2018-06-22 08:44:43 +0300

    Setting basic movement
    Character movement, prefabs and smoothed camera controller

---
+++ Suari Game/Assets/Scripts/CharacterHandler.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+
+public class CharacterHandler : MonoBehaviour {
+
+
+	private Rigidbody rb;
+
+	[HideInInspector]
+	public Vector3 currentSpeed;
+
+	[HideInInspector]
+	public float distanceToTarget;
+
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		rb.interpolation = RigidbodyInterpolation.Interpolate;
+		rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+		//physics material for character
+		if (GetComponent<Collider>().material.name == "Default (Instance)")
+		{
+			PhysicMaterial pMat = new PhysicMaterial();
+
+			pMat.name = "Frictionless";
+			pMat.frictionCombine = PhysicMaterialCombine.Multiply;
+			pMat.bounceCombine = PhysicMaterialCombine.Multiply;
+			pMat.dynamicFriction = 0f;
+			pMat.staticFriction = 0f;
+
+			GetComponent<Collider>().material = pMat;
+
+			Debug.LogWarning("Created physics material");
+		}
+	}
+
+	//move rigidbody to targetpos and return bool
+	public bool MoveTo(Vector3 destination, float acceleration, float stopDistance)
+	{
+		Vector3 relativePos = (destination - transform.position);
+
+		relativePos.y = 0;
+
+
+		//how far the target is
+		distanceToTarget = relativePos.magnitude;
+		//Debug.Log(distanceToTarget + " _dist");
+
+		if (distanceToTarget <= stopDistance)
+		{
+			//arrived to location
+			return true;
+		}
+		else
+		{
+			rb.AddForce(relativePos.normalized * acceleration * Time.deltaTime, ForceMode.VelocityChange);
+			return false;
+		}
+
+	}
+
+	//rotate rigidbody to characters velocity its going to
+	public void RotateToVelocity(float turnSpeed)
+	{
+		Vector3 direction;
+	
+		direction = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+		
+
+
+		if (direction.magnitude > 0.1)
+		{
+			Quaternion direction_Q = Quaternion.LookRotation(direction);
+			Quaternion slerp = Quaternion.Slerp(transform.rotation, direction_Q, direction.magnitude * turnSpeed * Time.deltaTime);
+
+			rb.MoveRotation(slerp);
+		}
+	}
+
+	//rotate rb to a set direction
+	public void RotateToDirection(Vector3 lookDir, float turnSpeed)
+	{
+		Vector3 charPos = transform.position;
+
+		charPos.y = 0;
+		lookDir.y = 0;
+
+		Vector3 charFacingDir = lookDir - charPos;
+		Quaternion charFacingDir_Q = Quaternion.LookRotation(charFacingDir);
+		Quaternion slerp = Quaternion.Slerp(transform.rotation, charFacingDir_Q, turnSpeed * Time.deltaTime);
+
+		rb.MoveRotation(slerp);
+	}
+
+	//friction to rigidbody and not going past of max allowed speed
+	public void ManageSpeed(float deceleration, float maxSpeed)
+	{ 
+
+		currentSpeed = rb.velocity;
+		
+		currentSpeed.y = 0;
+		
+
+		if (currentSpeed.magnitude > 0)
+		{
+			rb.AddForce((currentSpeed * -1) * deceleration * Time.deltaTime, ForceMode.VelocityChange);
+
+			if (rb.velocity.magnitude > maxSpeed)
+			{
+				rb.AddForce((currentSpeed * -1) * deceleration * Time.deltaTime, ForceMode.VelocityChange);
+			}
+		}
+	}
+
+}
---
+++ Suari Game/Assets/Scripts/PlayerCharacterMove.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerCharacterMove : MonoBehaviour
+{
+	[Header("References")]
+	public Camera mainCam;			
+	private Rigidbody rb;
+	private CharacterHandler characterHandler;
+
+	public Transform groundCheckPos;            //positions from where raycasting down to check if the player is grounded
+	private Transform[] groundCheckers;
+	private Quaternion screenMovementSpace;
+
+
+	[Header("Movement")]
+	public float maxMoveSpeed = 10;                         //moving on X and Z axis, as in ground
+	private Vector3 direction;
+	private Vector3 moveDirection;
+	private Vector3 screenMovementForward;
+	private Vector3 screenMovementRight;
+
+	private Vector3 movingObjSpeed;
+
+	//editor values overwrites set values, check code for originals
+	[SerializeField]
+	private bool isGrounded;
+	
+
+	private float curAccel;
+	private float curDecel;
+	private float curRotateSpeed;
+
+	public float groundAcceleration = 100f;
+	public float groundDeceleration = 10f;
+
+	public float AirAcceleration = 20f;
+	public float airAcceleration = 2f;
+
+	[Range(0f, 4f)]
+	public float rotateSpeed = 1f;
+	public float airRotateSpeed = 0.25f;
+
+	[Header("Slope Settings")]
+	public float allowedSlopeAngle = 50;					//if slope angle is higher then you...
+	public float slideAmount = 40;                          //will slide as fast as this down
+	private float calculatedSlopeAngle;
+
+	//jumping
+	public Vector3 jumpForce = new Vector3(0, 15, 0);		//normal jump force, editable
+	public float jumpMargin = 0.1f;							//height before hitting ground and you can still jump
+
+	//counters
+	private float airPressTime;
+	private float groundedCount;
+
+
+
+
+	void Awake()
+	{
+		if (!groundCheckPos)
+		{
+			Debug.LogWarning("NOTICE: No groundchecking found!");
+		}
+
+		//set tag for a player and camera to avoid editor changes
+		if (tag != "Player")
+		{
+			tag = "Player";
+			Debug.LogWarning("Player had no tag so it has been set");
+		}
+
+		mainCam = Camera.main;
+
+		if (mainCam.tag != "MainCamera")
+		{
+			tag = "MainCamera";
+			Debug.LogWarning("Camera had no tag so it has been set");
+		}
+
+		characterHandler = GetComponent<CharacterHandler>();
+		rb = GetComponent<Rigidbody>();
+
+		//get child objects of groundCheckPos, used to ray downwards to check isGrounded
+		groundCheckers = new Transform[groundCheckPos.childCount];
+
+		for (int i = 0; i < groundCheckers.Length; i++)
+		{
+			groundCheckers[i] = groundCheckPos.GetChild(i);
+		}
+	}
+
+
+	void Update()
+	{
+		//waking rb for continuous coll checks
+		rb.WakeUp();
+
+		JumpProcess();
+
+		//movement and rotates in air/ground
+		if (isGrounded)
+		{
+			curAccel = groundAcceleration;
+			curDecel = groundDeceleration;
+			curRotateSpeed = rotateSpeed;
+		}
+		else
+		{
+			curAccel = AirAcceleration;
+			curDecel = airAcceleration;
+			curRotateSpeed = airRotateSpeed;
+		}
+
+
+		//get movement axis relative to camera
+		screenMovementSpace = Quaternion.Euler(0, mainCam.transform.eulerAngles.y, 0);
+		screenMovementForward = screenMovementSpace * Vector3.forward;
+		screenMovementRight = screenMovementSpace * Vector3.right;
+
+		//get inputs
+		float horInput = Input.GetAxisRaw("Horizontal");
+		float verInput = Input.GetAxisRaw("Vertical");
+
+		//horizontal and vertical input
+		direction = (screenMovementForward * verInput) + (screenMovementRight * horInput);
+		moveDirection = transform.position + direction;
+	}
+
+	//fixedUpdate syncs with physics
+	void FixedUpdate()
+	{
+		isGrounded = IsGrounded();
+
+		//moving and rotating player
+		characterHandler.MoveTo(moveDirection, curAccel, 0.7f);
+
+		if (rotateSpeed != 0 && direction.magnitude != 0)
+		{
+			characterHandler.RotateToDirection(moveDirection, curRotateSpeed * 5);
+		}
+
+		characterHandler.ManageSpeed(curDecel, maxMoveSpeed + movingObjSpeed.magnitude);
+
+	}
+
+	//do not allow rb to slide on slopelimit
+	void OnCollisionStay(Collision other)
+	{
+		//only stop movement on slopes
+		if (other.collider.tag != "Untagged" || isGrounded == false)
+		{
+			return;
+		}
+		//no movement, stop player
+		if (direction.magnitude == 0 && calculatedSlopeAngle < allowedSlopeAngle && rb.velocity.magnitude < 2)
+		{
+			rb.velocity = Vector3.zero;
+		}
+	}
+
+
+	private bool IsGrounded()
+	{
+		//get distance to ground
+		float dist = GetComponent<Collider>().bounds.extents.y;
+
+		foreach (Transform gCheckObj in groundCheckers)
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(gCheckObj.position, Vector3.down, out hit, dist + 0.05f))
+			{
+				//Debug.DrawLine(gCheckObj.position, Vector3.down, Color.green, dist + 0.05f);
+				if (hit.transform.GetComponent<Collider>().isTrigger == false)
+				{
+					//calculate angle
+					calculatedSlopeAngle = Vector3.Angle(hit.normal, Vector3.up);
+					
+					if (calculatedSlopeAngle > allowedSlopeAngle)
+					{
+						Vector3 slide = new Vector3(0f, -slideAmount, 0f);
+						rb.AddForce(slide, ForceMode.Force);
+					}
+
+					//groundcheck raycast hit
+					return true;
+				}
+			}
+		}
+
+		movingObjSpeed = Vector3.zero;
+
+		//groundcheck raycast did not detect anything
+		return false;
+	}
+
+
+	private void JumpProcess()
+	{
+		//keep how long we have been on the ground
+		if (isGrounded)
+		{
+			groundedCount = groundedCount * Time.deltaTime;
+		}
+		else
+		{
+			groundedCount = 0f;
+		}
+
+		//buttonpress time while on air, used to allow jumping on air with margin
+		if (Input.GetButtonDown("Jump") && !isGrounded)
+		{
+			airPressTime = Time.time;
+		}
+
+		//grounded and allowing slopelimit 
+		if (isGrounded && calculatedSlopeAngle < allowedSlopeAngle)
+		{
+			if (Input.GetButtonDown("Jump") || airPressTime + jumpMargin > Time.time)
+			{
+				Jump(jumpForce);
+			}
+		}
+	}
+
+	public void Jump(Vector3 jumpPower)
+	{
+		rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+		rb.AddRelativeForce(jumpPower, ForceMode.Impulse);
+
+		airPressTime = 0f;
+	}
+
+}
